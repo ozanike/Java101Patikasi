861bed5edecdc621eb430013fcd4c001ab5f651e
Ozan İKE <ozanike@gmail.com>
2023-01-26 02:16:35 +0300

    Java101 Patikası burcBulma Ödev12...

---
+++ src/Giris/burcBulma.java
@@ -0,0 +1,145 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class burcBulma {
+    public static void main(String[] args) {
+
+        int month, day;
+        boolean isError = false;
+        String burc = "";
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Doğduğunuz Ay: ");
+        month = input.nextInt();
+        System.out.print("Doğduğunuz Gün: ");
+        day = input.nextInt();
+
+        if (month == 1) {
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    burc = "Oğlak";
+                } else {
+                    burc = "Kova";
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month == 2) {
+            if (day >= 1 && day <= 28) {
+                if (day < 20) {
+                    burc = "Kova";
+                } else {
+                    burc = "Balık";
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month == 3) {
+            if (day >= 1 && day <= 31) {
+                if (day < 21) {
+                    burc = "Balık";
+                } else {
+                    burc = "Koç";
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month == 4) {
+            if (day >= 1 && day <= 30) {
+                if (day < 21) {
+                    burc = "Koç";
+                } else {
+                    burc = "Boğa";
+                }
+            }
+        } else if (month == 5) {
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    burc = "Boğa";
+                } else {
+                    burc = "İkizler";
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month == 6) {
+            if (day >= 1 && day <= 30) {
+                if (day < 23) {
+                    burc = "İkizler";
+                } else {
+                    burc = "Yengeç";
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month == 7) {
+            if (day >= 1 && day <= 31) {
+                if (day < 23) {
+                    burc = "Yengeç";
+                } else {
+                    burc = "Aslan";
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month == 8) {
+            if (day >= 1 && day <= 31) {
+                if (day < 23) {
+                    burc = "Aslan";
+                } else {
+                    burc = "Başak";
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month == 9) {
+            if (day >= 1 && day <= 30) {
+                if (day < 23) {
+                    burc = "Başak";
+                } else {
+                    burc = "Terazi";
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month == 10) {
+            if (day >= 1 && day <= 31) {
+                if (day < 23) {
+                    burc = "Terazi";
+                } else {
+                    burc = "Akrep";
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month == 11) {
+            if (day >= 1 && day <= 30) {
+                if (day < 22) {
+                    burc = "Akrep";
+                } else {
+                    burc = "Yay";
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month == 12) {
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    burc = "Yay";
+                } else {
+                    burc = "Oğlak";
+                }
+            }
+        } else {
+            isError = true;
+        }
+        if (isError) {
+            System.out.println("Hatalı Gün veya Ay Girdiniz...");
+        } else {
+            System.out.println("Burcunuz: " + burc);
+        }
+
+    }
+}
